8d20c422c60a8b4c72868b3e9d8fc4cad627a63a
mxbooll <mxbooll@gmail.com>
2020-05-13 20:31:04 +0500

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -26,12 +26,30 @@ module.exports = {
     }),
     new CopyPlugin([
       {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      }
+        from: path.resolve(__dirname, "src/favicon.ico"),
+        to: path.resolve(__dirname, "dist"),
+      },
     ]),
     new MiniCssExtractPlugin({
       filename: "bundle.[hash].css",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
